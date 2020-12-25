5b66f2d5f2b487c8f18a4ffeb2c4a6d16a1e759d
tangwei <tangwei_12580@163.com>
2020-12-25 18:05:29 +0800

    first commit

---
+++ src/main/java/com/tangv/common/util/DateUtil.java
@@ -0,0 +1,21 @@
+package com.tangv.common.util;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+/**
+ * author:   tangwei
+ * Date:     2020/12/25 10:34
+ * Description:
+ */
+public class DateUtil {
+
+    public static long getTimeStemp(LocalDateTime dateTime) {
+        return dateTime.toInstant(ZoneOffset.of("+8")).toEpochMilli();
+    }
+
+    public static long getNowStemp() {
+        return LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
+    }
+
+}
